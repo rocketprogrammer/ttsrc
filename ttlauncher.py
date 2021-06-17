6975bbfb6a11f791fe871166dc4fa1d6c0f83371
NathaanTFM <juniordelaure@gmail.com>
2021-06-18 00:06:58 +0200

    Want magic words

--- ttlauncher.py
+++ ttlauncher.py
@@ -9,6 +9,7 @@ from pandac.PandaModules import loadPrcFileData, VirtualFileSystem, Filename
 
 loadPrcFileData("", "color-bits 8 8 8\nalpha-bits 8");
 loadPrcFileData("", "default-server-constants 1\nfake-playtoken test\ngame-server 34.136.173.79\nverify-ssl 0\ntt-specific-login 1\nserver-version dev");
+loadPrcFileData("", "want-magic-words 1")
 
 vfs = VirtualFileSystem.getGlobalPtr()
 for phase in [3,3.5,4,5,5.5,6,7,8,9,10,11,12,13]:

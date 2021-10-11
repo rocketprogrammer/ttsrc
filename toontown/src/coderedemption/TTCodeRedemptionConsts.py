565d84646bc923a3dbd2eae33580e742e40b39dc
Rocket Programmer <rocketgamer580@gmail.com>
2021-10-10 23:58:44 -0700

    code-redemption: Fix consts

--- toontown/src/coderedemption/TTCodeRedemptionConsts.py
+++ toontown/src/coderedemption/TTCodeRedemptionConsts.py
@@ -1,14 +1,14 @@
 DefaultDbName = 'tt_code_redemption'
 
 RedeemErrors = Enum(
-    'Success, CodeDoesntExist, CodeIsExpired, CodeAlreadyRedeemed, AwardCouldntBeGiven, '
+    'Success, CodeDoesntExist, CodeIsInactive, CodeAlreadyRedeemed, AwardCouldntBeGiven, '
     'TooManyAttempts, SystemUnavailable, ')
 
 # for ~code response
 RedeemErrorStrings = {
     RedeemErrors.Success: 'Success',
     RedeemErrors.CodeDoesntExist: 'Invalid code',
-    RedeemErrors.CodeIsExpired: 'Code is expired',
+    RedeemErrors.CodeIsInactive: 'Code is inactive',
     RedeemErrors.CodeAlreadyRedeemed: 'Code has already been redeemed',
     RedeemErrors.AwardCouldntBeGiven: 'Award could not be given',
     RedeemErrors.TooManyAttempts: 'Too many attempts, code ignored',
